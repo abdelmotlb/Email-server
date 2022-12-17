64451d28c03b3c28af5d583d82d31a6e937c9417
Ahmed Hesham <ahmedhesham.1652001@gmail.com>
2022-12-18 00:32:28 +0200

    Add files via upload

---
+++ emailBackend/src/test/java/com/emailBackend/emailBackend/EmailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailBackend.emailBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
