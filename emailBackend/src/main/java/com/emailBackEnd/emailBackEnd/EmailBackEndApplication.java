1eadd469e8f5a1d422c4871cb67ee9cc020a5f93
abdelrahman mohamed mohamed abdelmotlb <91913879+abdelmotlb@users.noreply.github.com>
2022-12-31 20:16:05 +0200

    send messages to front

---
+++ emailBackend/src/main/java/com/emailBackEnd/emailBackEnd/EmailBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.emailBackEnd.emailBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailBackEndApplication.class, args);
+	}
+
+}
