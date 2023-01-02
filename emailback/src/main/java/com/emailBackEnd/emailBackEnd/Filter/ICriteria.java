627de3e6a797642165d6b00c0078969ba5a36406
Ahmed Hesham <ahmedhesham.1652001@gmail.com>
2023-01-02 21:44:33 +0200

    Delete emailback directory

--- emailback/src/main/java/com/emailBackEnd/emailBackEnd/Filter/ICriteria.java
+++
@@ -1,9 +0,0 @@
-package com.emailBackEnd.emailBackEnd.Filter;
-
-import com.emailBackEnd.emailBackEnd.Message;
-
-import java.util.List;
-
-public interface ICriteria {
-    public List<Message> meetCriteria(List<Message> messages,String target);
-}
